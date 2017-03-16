e7bf13d9ab34276a9fc6ae0dd2548f90cc9a1e57
Sufiyan07 <sufiyan@unicoconnect.com>
2017-03-16 19:33:49 +0530

    first commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,29 @@
+@extends('master')
+
+@section('title','| Create a Post')
+
+
+@section('content')
+<div class="container">
+
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <h1>Create New Post</h1>
+
+      {!! Form::open(array('route' => 'posts.store')) !!}
+        {{ Form::label('title','Title:') }}
+        {{ Form::text('title',null,array('class'=>'form-control')) }}
+
+        {{ Form::label('body','Body:') }}
+        {{ Form::textarea('body',null,array('class'=>'form-control')) }}
+
+        {{ Form::submit('Create Post',array('class'=>'btn btn-success btn-lg btn-block', 'style'=> 'margin-top:20px')) }}
+      {!! Form::close() !!}
+
+
+    </div>
+  </div>
+
+</div>
+
+@endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,46 @@
+@extends('master')
+
+@section('title', '| View Post')
+
+@section('content')
+  <div class="col-md-8">
+    <h1>{{ $post->title }}</h1>
+    <p class="lead">{{ $post->body }}</p>
+  </div>
+
+  <div class="col-md-4">
+    <div class="well">
+      <dl class="dl-horizontal">
+        <dt>Created At:</dt>
+        <dd>{{ date('M j, Y h:i a', strtotime($post->created_at)) }}</dd>
+      </dl>
+
+      <dl class="dl-horizontal">
+        <dt>Last Updated:</dt>
+        <dd>{{ date('M j, Y h:i a', strtotime($post->updated_at)) }}</dd>
+      </dl>
+
+      <hr>
+      <div class="row">
+        <div class="col-sm-6">
+          {!! Html::linkRoute('posts.edit','Edit',array($post->id),array('class'=>'btn btn-primary btn-block')) !!}
+        </div>
+
+        <div class="col-sm-6">
+          {!! Form::open(['route' => ['posts.destroy',$post->id],'method' => 'DELETE']) !!}
+          {!! Form::submit('Delete',['class' => 'btn btn-danger btn-block']) !!}
+        </div>
+
+      </div>
+
+      <div class="row all-post">
+        <div class="col-md-12 col-sm-12">
+          {!! Html::linkRoute('posts.index','<< See All Posts',null,array('class'=>'btn btn-default btn-block')) !!}
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+@endsection
